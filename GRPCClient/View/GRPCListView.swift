b4ea45ea31d41e2c8e53f9de755da018b5eae7ea
yuutetu <yuutetu@gmail.com>
2019-12-15 13:39:06 +0900

    reactorkit

--- GRPCClient/View/GRPCListView.swift
+++ GRPCClient/View/GRPCListView.swift
@@ -7,15 +7,17 @@
 //
 
 import SwiftUI
+import ReactorKit
+import RxSwift
 
-struct GRPCListView: View {
+struct GRPCListView: SwiftUI.View {
     @ObservedObject var viewModel: GRPCListViewModel
 
     init(viewModel: GRPCListViewModel) {
         self.viewModel = viewModel
     }
 
-    var body: some View {
+    var body: some SwiftUI.View {
         List {
             ForEach(viewModel.services, id: \.self) { service in
                 Section(header: Text(service.name)) {
@@ -29,6 +31,44 @@ struct GRPCListView: View {
 }
 
 class GRPCListViewModel: ObservableObject {
+    @Published var services: [GRPCListViewReactor.Service]
+    private var reactor: GRPCListViewReactor
+    private var disposeBag = DisposeBag()
+
+    init() {
+        self.services = []
+        self.reactor = GRPCListViewReactor()
+
+        self.setupReactor()
+    }
+
+    private func setupReactor() {
+        reactor.state.subscribe(onNext: { [weak self] state in
+            self?.services = state.services
+        }, onError: { error in
+            // error handling
+        }, onCompleted: {
+            // completion
+        }, onDisposed: {
+            // disposed
+        }).disposed(by: disposeBag)
+    }
+}
+
+class GRPCListViewReactor: Reactor {
+    var initialState = State(services: [
+        GRPCListViewReactor.Service(name: "NekoService", endpoints: [
+            GRPCListViewReactor.Endpoint(name: "CreateNeko"),
+            GRPCListViewReactor.Endpoint(name: "UpdateNeko")
+        ]),
+        GRPCListViewReactor.Service(name: "DogService", endpoints: [
+            GRPCListViewReactor.Endpoint(name: "CreateDog"),
+            GRPCListViewReactor.Endpoint(name: "UpdateDog"),
+            GRPCListViewReactor.Endpoint(name: "GetDog"),
+            GRPCListViewReactor.Endpoint(name: "ListDogs")
+        ])
+    ])
+
     struct Service: Hashable {
         var name: String
         var endpoints: [Endpoint]
@@ -37,15 +77,12 @@ class GRPCListViewModel: ObservableObject {
     struct Endpoint: Hashable {
         var name: String
     }
-    
-    @Published var services: [Service]
 
-    init() {
-        self.services = [
-            Service(name: "NekoService", endpoints: [
-                Endpoint(name: "CreateNeko"),
-                Endpoint(name: "UpdateNeko")
-            ])
-        ]
+    enum Action {
+        case create(serviceName: String, endpointName: String)
+    }
+
+    struct State {
+        var services: [Service]
     }
 }
